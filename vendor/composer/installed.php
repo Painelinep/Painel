34bee9c3b0ca8621fd508bd24f77b2b82324c471
Painelinep <rubens.rego@modulo.com>
2025-11-29 02:45:58 -0300

    Resgate Crítico: Adicionando Core do Zend Framework

---
+++ vendor/composer/installed.php
@@ -0,0 +1,401 @@
+<?php return array(
+    'root' => array(
+        'name' => 'zendframework/skeleton-application',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dino/dompdf-module' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '1b6b5b90090579239cb7d49363632ca6b2b35afb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../dino/dompdf-module',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'dompdf/dompdf' => array(
+            'pretty_version' => 'v0.8.3',
+            'version' => '0.8.3.0',
+            'reference' => '75f13c700009be21a1965dc2c5b68a8708c22ba2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../dompdf/dompdf',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'mobiledetect/mobiledetectlib' => array(
+            'pretty_version' => '2.8.45',
+            'version' => '2.8.45.0',
+            'reference' => '96aaebcf4f50d3d2692ab81d2c5132e425bca266',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mobiledetect/mobiledetectlib',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phenx/php-font-lib' => array(
+            'pretty_version' => '0.5.1',
+            'version' => '0.5.1.0',
+            'reference' => '760148820110a1ae0936e5cc35851e25a938bc97',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phenx/php-font-lib',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phenx/php-svg-lib' => array(
+            'pretty_version' => 'v0.3.3',
+            'version' => '0.3.3.0',
+            'reference' => '5fa61b65e612ce1ae15f69b3d223cb14ecc60e32',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phenx/php-svg-lib',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'sabberworm/php-css-parser' => array(
+            'pretty_version' => '8.3.0',
+            'version' => '8.3.0.0',
+            'reference' => '91bcc3e3fdb7386c9a2e0e0aa09ca75cc43f121f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../sabberworm/php-css-parser',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'zendframework/skeleton-application' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'zendframework/zend-authentication' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-barcode' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-cache' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-captcha' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-code' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-config' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-console' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-crypt' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-db' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-debug' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-di' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-dom' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-escaper' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-eventmanager' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-feed' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-file' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-filter' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-form' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-http' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-i18n' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-inputfilter' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-json' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-ldap' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-loader' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-log' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-mail' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-math' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-memory' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-mime' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-modulemanager' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-mvc' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-navigation' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-paginator' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-permissions-acl' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-permissions-rbac' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-progressbar' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-resources' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-serializer' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-server' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-servicemanager' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-session' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-soap' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-stdlib' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-tag' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-test' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-text' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-uri' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-validator' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-version' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-view' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zend-xmlrpc' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.4.13',
+            ),
+        ),
+        'zendframework/zendframework' => array(
+            'pretty_version' => '2.4.13',
+            'version' => '2.4.13.0',
+            'reference' => '06bf809653a51e843b7a84dc380ac1d8a0a6a1ea',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../zendframework/zendframework',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'zendframework/zendxml' => array(
+            'pretty_version' => '1.2.0',
+            'version' => '1.2.0.0',
+            'reference' => 'eceab37a591c9e140772a1470338258857339e00',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../zendframework/zendxml',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
